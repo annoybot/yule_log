0d79be7591941f878afc6c3c3bc01387891bb2a8
Patrick Nadeau <pnadeau@annoybot.org>
2024-10-30 08:08:06 -0400

    Removed sample test from `lib.rs`.

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,4 @@ pub mod parser;
 pub mod datastream;
 mod formats;
 mod tokenizer;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        
-        let a = "a";
-
-        
-        assert_eq!(result, 4);
-    }
-}
+mod csv;
